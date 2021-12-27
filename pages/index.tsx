12d949c8564900f20cba2a6b69e8b0723a282c90
Stefanos Lignos Zivver <stefanos.lignos@zivver.com>
2021-12-27 17:36:11 +0100

    Initial commit from Create Next App

---
+++ pages/index.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import Head from "next/head";
+import Container from "../components/container";
+
+function HomePage() {
+  return (
+    <>
+      <Head>
+        <title>Stefanos.dev 🚀</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Container>
+        <div className="space-y-6">
+          <h1 className="text-2xl font-bold">
+            Hey, I'm a Senior Software Engineer at Company. I enjoy working with
+            Next.js and crafting beautiful front-end experiences.
+          </h1>
+          <p>
+            This blog is built with Next.js.
+          </p>
+        </div>
+      </Container>
+
+      <div className="container max-w-4xl m-auto px-4 mt-20">
+        <Image
+          src="/desk.jpg"
+          alt="my desk"
+          width={1920 / 2}
+          height={1280 / 2}
+        />
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
