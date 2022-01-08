f1469562b03d204408bd9c3cf507ff9b1e0107eb
stefanoslig <stefanoslignos@gmail.com>
2022-01-08 20:00:19 +0100

    Fix some typos

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -9,7 +9,7 @@ function Posts() {
           January 2022.
         </h4>
         <h4>
-          Until then have a look on my account on
+          Until then have a look at my account on
           <a
             className="underline text-sky-500	text-lg rounded-lg"
             href="https://medium.com/@stefanoslignos"
