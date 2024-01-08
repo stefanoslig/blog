9592307000b2854cd964de6012dedd9b697ed511
stefanoslig <stefanoslignos@gmail.com>
2024-01-08 13:25:01 +0100

    fix: build error

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,11 +1,10 @@
 import { remark } from "remark";
 import html from "remark-html";
-import remarkPrism from "remark-prism";
 
 export default async function markdownToHtml(markdown: string) {
   const result = await remark()
     .use(html, { sanitize: false })
-    .use(remarkPrism, { plugins: ["line-numbers"] })
+    .use(require('remark-prism'), { plugins: ["line-numbers"] })
     .process(markdown);
 
   return result.toString();
